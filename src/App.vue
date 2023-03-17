835e91be96effae56e6cbda81c741e1c97c55ac8
CrystianACR <crystianescuela14@gmail.com>
2023-03-17 12:30:59 -0500

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import {RouterView} from 'vue-router';
+</script>
 <template>
+  <RouterLink to="/crear">CrearUsuario</RouterLink>
+  <RouterLink to="/listar">ListarUsuario</RouterLink>
   <RouterView />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ListarUsuario from "../components/ListarUsuario.vue"
+import CrearUsuario from "../components/CrearUsuario.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +27,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DashboardView.vue')
+    },
+    {
+      path: '/listar',
+      name: 'listar',
+      component: ListarUsuario
+    },
+    {
+        path: '/crear',
+        name: 'crear',
+        component: CrearUsuario
     }
   ]
 })
